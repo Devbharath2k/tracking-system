fb6a234326e19ba8243e4d31c61c53a3ac5a89bd
Devbharath2k <devbharath21k@gmail.com>
2025-05-17 12:23:56 +0530

    'frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='flex justify-center items-center pt-55 mt-33 text-2xl'>
+      <p className=''>welcome to code </p>
+    </div>
+  )
+}
+
+export default App
