329d582a08c40697091c69df5ee5e575e1c253a0
Devbharath2k <devbharath21k@gmail.com>
2025-05-19 04:11:19 +0530

    new-command

--- frontend/src/components/RecentTransctions.jsx
+++ frontend/src/components/RecentTransctions.jsx
@@ -1,26 +1,34 @@
 import React from "react";
-import { LuUtensils, LuTrendingUp, LuTrendingDown, LuTrash2 } from "react-icons/lu";
+import { LuArrowRight } from "react-icons/lu";
+// import moment from 'momen
+import TransactionInfoCard from "./TransactionInfoCard"; // Make sure this import exists
 
-function TransactionInfoCard({ title, icon, date, amount, type, hideDeleteBtn }) {
+function RecentTransctions({ transactions, onSeeMore }) {
   return (
-    <div className="group relative flex items-center gap-4 mt-3 p-3 rounded-lg hover:bg-gray-100/60">
-      <div className="w-12 h-12 flex justify-center items-center text-xl text-gray-200 bg-gray-100 rounded-full">
-        {icon ? <img src={icon} alt={title} className="w-6 h-6" /> : <LuUtensils />}
+    <div>
+      <div className="card">
+        <div className="flex items-center justify-between">
+          <h4 className="text-lg">Recent Transctions</h4>
+          <button className="card_btn" onClick={onSeeMore}>
+            see all <LuArrowRight />
+          </button>
+        </div>
+        <div className="mt-6">
+          {transactions?.slice(0, 5)?.map((item) => (
+            <TransactionInfoCard
+              key={item._id}
+              title={item.type === "expense" ? item.category : item.source}
+              icon={item.icon}
+              date={moment(item.date).format("DD MMM YYYY")}
+              amount={item.amount}
+              type={item.type}
+              hideDeleteBtn
+            />
+          ))}
+        </div>
       </div>
-      <div className="flex-1">
-        <h5 className="text-sm font-semibold">{title}</h5>
-        <p className="text-xs text-red-500">{date}</p>
-      </div>
-      <div className={`text-sm font-semibold ${type === "income" ? "text-green-600" : "text-red-600"}`}>
-        {type === "income" ? "+" : "-"}${amount}
-      </div>
-      {!hideDeleteBtn && (
-        <button className="text-red-500 opacity-0 group-hover:opacity-100 transition-opacity duration-200">
-          <LuTrash2 />
-        </button>
-      )}
     </div>
   );
 }
 
-export default TransactionInfoCard;
+export default RecentTransctions;
