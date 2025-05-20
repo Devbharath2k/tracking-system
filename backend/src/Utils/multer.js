6a158c1eaefee4c86c791c7d30b77dc502af73e4
Devbharath2k <devbharath21k@gmail.com>
2025-05-20 11:07:31 +0530

    'new-command'

--- backend/src/Utils/multer.js
+++
@@ -1,9 +0,0 @@
-import multer from "multer";
-
-const storage = multer.memoryStorage();
-
-const profilephotoUpload = multer({storage}).single('profilephoto');
-
-const LogoUpload = multer({storage}).single('logo');
-
-export {profilephotoUpload, LogoUpload};
